89a7eebfa988455c3fdb56506a856a666280f1dc
Alexander <pchp250g8@tutamail.com>
2026-02-06 11:05:18 +0300

    Добавьте файлы проекта.

---
+++ IsNumberEven/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input an integer positive number");
+UInt64.TryParse(Console.ReadLine(), out ulong ulNumber);
+if (ulNumber % 2 == 0)
+    Console.WriteLine("The number is even");
+else
+    Console.WriteLine("The number is odd");
+Console.ReadLine();
